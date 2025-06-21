b18a6cdd361234c13e541b0b45bed00e8085bb72
Fliptoss <samirman.0009@gmail.com>
2025-06-21 18:47:04 +0600

    first commit

---
+++ const.js
@@ -0,0 +1,19 @@
+const a1 = {
+  name: "Harry",
+  section: 1,
+  isPrincipal: false,
+};
+
+a1["friend"] = "shubham";
+//change the name object
+a1["name"] = "sameer";
+
+console.log(a1);
+
+const dict = {
+  appretiate: "recognize the full worth of",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work",
+};
+
+console.log(dict.yakka);
