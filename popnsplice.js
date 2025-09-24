77fdee37e1d99d70021ef00406f9d14d79f1f31c
Fliptoss <samirman.0009@gmail.com>
2025-09-24 22:21:17 +0600

    pop and slice

---
+++ popnsplice.js
@@ -0,0 +1,8 @@
+let courses = ["html", "css", "js", "reactjs", "jquery"];
+
+console.log(courses);
+let removeItems = courses.pop();
+console.log(courses);
+courses.pop();
+courses.pop();
+console.log(courses);
