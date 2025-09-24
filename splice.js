2ff18e781fb6499898177fcf91fb296d73829b3d
Fliptoss <samirman.0009@gmail.com>
2025-09-24 22:26:47 +0600

    changes to splice

--- splice.js
+++ splice.js
@@ -2,3 +2,6 @@ let courses = ["html", "css", "js", "reactjs", "angular", "vuejs", "nextjs"];
 
 courses.splice(4, 2);
 console.log(courses);
+
+courses.splice(3, 0, "nestjs");
+console.log(courses);
