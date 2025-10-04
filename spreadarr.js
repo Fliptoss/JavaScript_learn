b227703c9f536acd9ae899f761dec1c2ef4c7bb2
Fliptoss <samirman.0009@gmail.com>
2025-10-04 20:23:27 +0600

    more spread

--- spreadarr.js
+++ spreadarr.js
@@ -1,3 +1,8 @@
 const arr = [1, 2, 3, 4, 5, 6];
 console.log("array: ", arr);
 console.log(...arr);
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8];
+const arr3 = [...arr1, ...arr2, 9, 10];
+console.log(arr3);
